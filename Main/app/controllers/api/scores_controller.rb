5d8f224a50e3a84a0bdc9391574a707d653f429a
Terrell Quillin <terrellq@gmail.com>
2016-02-15 16:36:46 -0700

    Works in controllers files.

--- Main/app/controllers/api/scores_controller.rb
+++ Main/app/controllers/api/scores_controller.rb
@@ -1,2 +1,12 @@
 class Api::ScoresController < ApplicationController
+
+  def index
+  end
+
+  if params[:search]
+      search_term = params[:search]
+      scores = Score.where("admin ILIKE ?", "%#{search_term}%")
+  end
+  render json: {scores: scores}
+
 end
