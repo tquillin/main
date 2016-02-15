ea4e38998305718c5c1e76b455a4277b23928749
Terrell Quillin <terrellq@gmail.com>
2016-02-15 16:00:26 -0700

    runs rails g controller welcome index and adds angular cdn google into layouts application.

--- Main/config/routes.rb
+++ Main/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   root 'welcome#index'
 
   namespace :api do
     resources :scores
-  end 
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
